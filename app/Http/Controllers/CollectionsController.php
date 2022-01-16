2ba79b0451c8fdbc4846ec8823c418bfe5453626
Dani <danixidev@gmail.com>
2022-01-17 00:58:26 +0100

    Update CollectionsController.php

--- app/Http/Controllers/CollectionsController.php
+++ app/Http/Controllers/CollectionsController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Card;
 use App\Models\Collection;
+use App\Models\Contain;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class CollectionsController extends Controller
 {
@@ -17,6 +17,7 @@ class CollectionsController extends Controller
             'name' => 'required|unique:collections|string',
             'symbol' => 'required|string',
             'release_date' => 'required|date_format:Y-m-d',
+            'card_name' => 'required|date_format:Y-m-d',
         ], [
             'date_format' => 'El formato no coincide con YYYY-MM-DD (1999-03-25)',
         ]);
@@ -30,15 +31,24 @@ class CollectionsController extends Controller
 
             $data = json_decode($data);
             try {
-                $collection = new Collection();
+                $card = Card::where('name', $data->card_name)->first();
+                if($card) {
+                    $collection = new Collection();
+                    $collection->name = $data->name;
+                    $collection->symbol = $data->symbol;
+                    $collection->release_date = $data->release_date;
+                    $collection->save();
 
-                $collection->name = $data->name;
-                $collection->symbol = $data->symbol;
-                $collection->release_date = $data->release_date;
+                    $contain = new Contain();
+                    $contain->card_id = $card->id;
+                    $contain->collection_id = $collection->id;
+                    $contain->save();
 
-                $collection->save();
-
-                $response['msg'] = "Coleccion creada correctamente con el id ".$collection->id;
+                    $response['msg'] = "Coleccion creada correctamente con el id ".$collection->id;
+                } else {
+                    $response['msg'] = "No existe ninguna carta con ese nombre";
+                    $response['status'] = 0;
+                }
             } catch (\Throwable $th) {
                 $response['msg'] = "Se ha producido un error:".$th->getMessage();
                 $response['status'] = 0;
