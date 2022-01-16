214b3e6e0adf6270bae3e194a8ed628092d55320
Dani <danixidev@gmail.com>
2022-01-16 13:12:32 +0100

    Create migrations

--- database/migrations/2022_01_15_190550_create_offers_table.php
+++ database/migrations/2022_01_15_190550_create_offers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateOffersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('offers', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['particular', 'profesional', 'administrador']);
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('card_id')->constrained('cards');
+            $table->integer('amount');
+            $table->integer('price');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('offers');
     }
 }
