1ad11540ab6f385aa6b7cc66bb8ff64699d0f623
Dani <danixidev@gmail.com>
2022-01-16 13:39:40 +0100

    Add api token to user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->enum('role', ['particular', 'profesional', 'administrador']);
+            $table->string('api_token')->unique()->nullable();
             $table->timestamps();
         });
     }
