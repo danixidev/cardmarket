7e0098f23dd707e4db2793cc492bdce33c22b211
Dani <92784534+danixidev@users.noreply.github.com>
2022-03-02 19:29:15 +0100

    Create users test

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    public function test_noName()
+    {
+        $response = $this->put(
+            '/api/login',
+            [
+                "username" => "",
+                "password" => "Daniel1"
+            ]
+        );
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => 0,
+            ]);
+
+        // var_dump($response->original);
+    }
+    public function test_incorrectNamePassword()
+    {
+        $response = $this->put(
+            '/api/login',
+            [
+                "username" => "daniel",
+                "password" => "Password"
+            ]
+        );
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => 0,
+            ]);
+
+        // var_dump($response->original);
+    }
+    public function test_correctNamePassword()
+    {
+        $response = $this->put(
+            '/api/login',
+            [
+                "username" => "dani",
+                "password" => "Daniel1"
+            ]
+        );
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => 1,
+            ]);
+
+        // var_dump($response->original);
+    }
+}
